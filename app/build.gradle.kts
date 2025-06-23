a4a5ec8827c65b2bc8762fa91a611c9ffb3f0bae
objectiveCarlo <carloluismb@mediacorp.com.sg>
2025-06-23 16:37:10 +0800

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,15 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+    implementation(libs.gson)
+
+    implementation(libs.lifecycle.viewmodel)
+    implementation(libs.lifecycle.livedata)
+
+    implementation(libs.coroutines.android)
+
+    implementation(libs.okhttp.logging)
 }
